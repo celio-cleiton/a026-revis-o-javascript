7e38019b4b96f1f60f30f99ef019455baf6cb3b8
Tayanne <tayh.snovaes@gmail.com>
2022-09-22 20:55:24 -0300

    conserta a rota de redirecionamento

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-function logar(params) {
-    var email = document.getElementById('email').value;
-    var senha = document.getElementById('password').value;
+function logar() {
+  var email = document.getElementById('email').value
+  var senha = document.getElementById('password').value
 
-    if (email === "admin@gmail.com" && senha === "admin") {
-        alert("Sucessfully logged in")
-        window.location.href="http://127.0.0.1:5500/sistemapostinhojs.html";
-    } else {
-        alert("Incorrect Login or Password")
-    }
+  if (email === 'admin@gmail.com' && senha === 'admin') {
+    window.location.href = '/pages/sistema.html' // o caminho no navegador
+    alert('Sucessfully logged in')
+  } else {
+    alert('Incorrect Login or Password')
+  }
 }
